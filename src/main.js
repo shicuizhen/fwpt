d9bcd940cef331636c8a25be1a5b94d8b7503dc3
shicuizhen <17692151418@163.com>
2020-12-03 09:40:52 +0800

    回答部分-未完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/base.css'
 import './assets/fonts/iconfont.css'
+import './assets/css/question.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
--- src/views/question/Question.vue
+++ src/views/question/Question.vue
@@ -47,24 +47,62 @@
     </div>
     <div class="cen2">
       <div class="left">
-        <p>平台最新问题</p>
-        <div class="sort_xlk">
+        <div class="search">
+          <p>平台最新问题</p>
           <a href="" >选择分类</a>
           <i @click="icon_down()" class="el-icon-arrow-down" :class="{hidden : downState}" aria-hidden="true"></i>
           <i @click="icon_up()" class="el-icon-arrow-up" :class="{hidden : state}" aria-hidden="true"></i>
           <ul :class="{xlk : true, hidden : state}">
-            <!--          <span class="el-icon-caret-top" aria-hidden="true"></span>-->
             <li>全部</li>
             <li>生活类问题</li>
             <li>学习类问题</li>
             <li>退出</li>
           </ul>
+          <input type="text" value="搜索" onfocus="if (value == '搜索'){value = ''}" onblur="if (value == ''){value = '搜索'}">
+          <div class="no_solve butt">
+            仅看未解决&nbsp;
+            <el-button type="success" icon="el-icon-check" circle></el-button>
+          </div>
         </div>
-        <input type="text" value="搜索" onfocus="if (value == '搜索'){value =''}" onblur="if (value ==''){value='搜索'}">
-        <div class="no_solve butt">
-          仅看未解决&nbsp;
-          <el-button type="success" icon="el-icon-check" circle></el-button>
-        </div>
+        <ul class="list">
+          <li class="que">
+            <h4>这是一个问题的标题！</h4>
+            <span><img src="../../assets/images/1.jpeg" alt=""></span>
+            <span>用户甲</span>
+            <span>一个小时前发布</span>
+            <span class="que_sort">问题类别</span>
+            <span class="iconfont">&#xe775;</span>
+            <p>问题的内容，瑞丰园大赛范德萨胡椒粉电话就卡了,问题的内容，瑞丰园大 范德萨胡椒粉电话就卡了问题的内容，瑞丰园大aohj 范德萨胡椒粉电话就卡了问题的内容，瑞丰园大赛范德萨胡椒粉电话就卡了问题的内容，瑞丰园大j 范德萨胡椒粉电话就卡了问题的内容，瑞丰园大赛ufyapsydfuiysuiayfduiysaohj 范德萨胡椒粉电话就卡了</p>
+            <div class="que_num">回答 (231)</div>
+
+            <ul class="reply">
+              <span class="el-icon-caret-top" aria-hidden="true"></span>
+<!--              <input type="text">-->
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <li>
+                <p>回答的具体内容：这是一个回答，可以有图片的那种</p>
+                <span><img src="../../assets/images/1.jpeg" alt=""></span>
+                <span>用户甲</span>
+                <span>一个小时前发布</span>
+                <button>评论（322）</button>
+                <span class="iconfont">&#xe775;</span>
+<!--                <ul class="comm">-->
+<!--                  <span class="el-icon-caret-top" aria-hidden="true"></span>-->
+<!--                  <input type="text" value="这是一个评论。。。">-->
+<!--                  <li>-->
+<!--                    <span><img src="../../assets/images/1.jpeg" alt=""></span>-->
+<!--                    <span>用户甲</span>-->
+<!--                    <p>评论的具体内容：人情味儿无群而我却热完全而我却人情味热王企鹅无群</p>-->
+<!--                    <span>一个小时前发布</span>-->
+<!--                    <span><button>回复</button></span>-->
+<!--                    <span class="iconfont">&#xe775;</span>-->
+<!--                  </li>-->
+<!--                </ul>-->
+              </li>
+            </ul>
+
+          </li>
+        </ul>
       </div>
       <div class="right">
       </div>
@@ -96,150 +134,75 @@ export default {
 </script>
 
 <style scoped>
-  /*上部top---开始*/
-  .top{
-    position: relative;
-    height: 150px;
-    width: 100%;
-    background-color: #BBD8FF;
+  /*中间-左侧-问题展示列表---开始*/
+  /*问题部分*/
+  .left .list{
   }
-  .top>a{
-    position: absolute;
-    top: 30px;
-    left: 35%;
-    height: 36px;
-    line-height: 36px;
-    width: 86px;
-    text-align: center;
-    background-color: #fff;
-    color: #525252;
-    font-size: 14px;
+  .left .list .que{
+    background-color: #8DD6E0;
+    padding: 10px 10px;
+    overflow: hidden;
+    margin-top: 10px;
   }
-  .top>a:hover{
-    color: #BBD8FF;
+  .que h4{
+    font-family: 微软雅黑;
+    font-size: 16px;
+    margin-bottom: 10px;
   }
-  /*分类轮播*/
-  .top .sort{
-    position: absolute;
-    top: 98px;
-    /*margin: 0 4.3%;*/
-    width: 100%;
-  }
-  .sort li{
-    float: left;
-    background-color: #FFFFFF;
-    height: 90px;
-    width: 17.1%;
-    margin-right: 1.3%;
-    border: 1px solid #C1C1C1;
-    display: inline-block;
-  }
-  .sort li:nth-child(5){
-    margin-right: 0;
-  }
-  .sort a{
-    display: inline-block;
-    width: 100%;
-    height: 100%;
-  }
-  .sort li span{
-    font-size: 12px;
-    margin-top: 8px;
-    margin-left: 15px;
-    display: inline-block;
-  }
-  .sort li span:nth-child(1){
-    color: #90CE94;
-  }
-  .sort li span:nth-child(4){
-    float: right;
-    padding-right: 15px;
-  }
-  .sort li p{
-    text-align: center;
-    height: 30px;
-    line-height: 30px;
-  }
-  /*上部top结束*/
-  /*搜索部分search---开始*/
-  .left{
-    position: relative;
-    height: 42px;
-    background-color: #D3EBD5;
-    margin-top: 52px;
-  }
-  .left p{
-    /*margin-left: 15px;*/
-    margin-left: 1.2%;
-    line-height: 42px;
-  }
-  /*下拉框*/
-  .sort_xlk>a{
-    position: absolute;
-    /*left: 150px;*/
-    left: 12.8%;
-    top: 0;
-    line-height: 42px;
-    font-size: 14px;
-  }
-  .sort_xlk>i{
-    position: absolute;
-    /*left: 210px;*/
-    left: 18.6%;
-    top: 15px;
-    font-size: 12px;
-    font-weight: bold;
-  }
-  .hidden{
-    display: none;
-  }
-  .xlk{
-    background-color: #F5F5F5;
-    position: absolute;
-    top: 38px;
-    /*left: 136px;*/
-    left: 10.7%;
-    width: 90px;
-  }
-  .xlk li{
-    text-align: center;
-    border: 1px solid #C6C6C6;
-    color: #101010;
-    line-height: 32px;
-    font-size: 12px;
-  }
-  .xlk li:hover{
-    background-color: #F9F9F9;
-  }
-  .xlk li:hover{
-    font-weight: bold;
-  }
-  /*搜索框*/
-  .left input{
-    height: 22px;
-    width: 180px;
-    position: absolute;
-    top: 9px;
-    left: 240px;
-    outline: #fff;
-    border: 1px solid #fff;
-    color: #888888;
-    font-size: 12px;
-    padding: 0 5px;
-  }
-  /*仅看未解决*/
-  /*.no_solve */
-  .no_solve .el-button.is-circle {
+  .que img{
+    width: 20px;
+    height: 20px;
     border-radius: 50%;
-    padding: 2px;
   }
-  .no_solve{
-    position: absolute;
-    top: 12px;
-    right: 26px;
+  .que span{
+    float: left;
+    margin-right: 20px;
     font-size: 14px;
+    line-height: 22px;
   }
-  /*搜索部分search---结束*/
-  /*中间部分center---开始*/
-  /*中间部分center---结束*/
+  .que .que_sort{
+    border: 1px solid #fff;
+    padding: 2px 5px;
+    line-height: 16px;
+  }
+  .que .iconfont{
+    font-size: 22px;
+    float: right;
+  }
+  .que p{
+    /*清除前面元素的浮动*/
+    clear: both;
+    padding-top: 5px;
+    line-height: 25px;
+    letter-spacing: 1px;
+  }
+  .que .que_num{
+    padding: 5px 8px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 12px;
+    background-color: #72C076;
+    border: 1px solid #72C076;
+    display: inline-block;
+  }
+  .que .que_num:hover{
+    border: 1px solid #fff;
+  }
+  /*回答部分*/
+  .que .reply{
+    background-color: orange;
+    position: relative;
+  }
+  .reply .el-icon-caret-top{
+    position: absolute;
+    top: -15px;
+    left: 30px;
+  }
+  .reply textarea{
+    height: 26px;
+    width: 100%;
+    line-height: 26px;
+    text-indent:2em;
+  }
+  /*中间-左侧-问题展示列表---结束*/
 </style>
