78044a7562076f455729a61672c6254ca0219674
shicuizhen <17692151418@163.com>
2021-04-20 17:04:48 +0800

    后台管理用户信息编辑

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -86,7 +86,7 @@
         </el-table>
       </el-tab-pane>
       <el-tab-pane label="用户信息">
-        <el-table
+        <el-table  v-if="uid === null"
           :data="UserData"
           style="width: 100%"
           :default-sort = "{prop: 'date', order: 'descending'}">
@@ -153,6 +153,82 @@
             </template>
           </el-table-column>
         </el-table>
+        <div  v-if="uid !== null">
+          <div class="fanhui" @click="handleEditUser(null,null)">返回</div>
+          <el-form :model="form" :rules="rules" ref="form" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="学号" prop="sno">
+              <el-input v-model="form.sno"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名" prop="name">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="昵称" prop="nick">
+              <el-input v-model="form.nick"></el-input>
+            </el-form-item>
+            <el-form-item label="头像">
+              <div class="form-group">
+                <div class="control-form">
+                  <p class="help-block">(建议图片格式为：JPEG/BMP/PNG/GIF，大小不超过5M)</p>
+                  <ul class="upload-imgs">
+                    <li v-if="imgLen>1 || imgLen===1 ? false : true">
+                      <input type="file" class="upload" @change="addImg" ref="inputer" multiple accept="image/png,image/jpeg,image/gif,image/jpg"/>
+                      <a class="add">
+                        <i class="iconfont icon-plus"></i><p>点击上传</p>
+                      </a>
+                    </li>
+                    <li v-for='(value, key) in imgs' v-bind:key="key">
+                      <p class="img"><img :src="getObjectURL(value)"><a class="close" @click="delImg(key)">×</a></p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </el-form-item>
+            <el-form-item class="form_type" label="性别" prop="sex">
+              <el-radio-group v-model="form.sex">
+                <el-radio label="0">男</el-radio>
+                <el-radio label="1">女</el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="生日" prop="birthday">
+              <el-col :span="11">
+                <el-form-item>
+                  <el-date-picker type="date" placeholder="选择日期" v-model="form.birthday" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="年级" prop="grade">
+              <el-select v-model="form.grade" placeholder="请选择年级">
+                <el-option :label=item.name :value=item.name v-for="item in grades" v-bind:key="item.name"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="院系" prop="college">
+              <el-select v-model="form.college" placeholder="请选择院系">
+                <el-option :label=item.college :value=item.id v-for="item in colleges" v-bind:key="item.college"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item v-if="form.college" label="专业" prop="major">
+              <el-select v-model="form.major" placeholder="请选择专业">
+                <el-option :label=item.major :value=item.id v-for="item in majors" v-bind:key="item.major"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="手机号码" prop="phone">
+              <el-input type="tel" v-model.number="form.phone"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+              <el-input type="email" v-model="form.email"></el-input>
+            </el-form-item>
+            <!--      <el-form-item label="邮箱" prop="email">-->
+            <!--        <el-input type="email" v-model.number="form.email"></el-input>-->
+            <!--      </el-form-item>-->
+            <el-form-item label="密码" prop="password">
+              <el-input type="password" v-model="form.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('form')">提交</el-button>
+              <el-button @click="resetForm('form')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
       </el-tab-pane>
       <el-tab-pane label="咨询信息">
         <el-table
@@ -311,6 +387,16 @@
 
 <script>
 import axios from 'axios'
+import { isvalidPhone } from '../config/validate'
+var validPhone = (rule, value, callback) => {
+  if (!value) {
+    callback(new Error('请输入电话号码'))
+  } else if (!isvalidPhone(value)) {
+    callback(new Error('请输入正确的11位手机号码'))
+  } else {
+    callback()
+  }
+}
 export default {
   name: 'Manage',
   data: function () {
@@ -329,7 +415,82 @@ export default {
       major: null,
       quesMsg: {},
       lostMsg: {},
-      moodMsg: {}
+      moodMsg: {},
+      uid: null,
+      //  用户信息编辑
+      grades: [
+        { name: '17' },
+        { name: '18' },
+        { name: '19' },
+        { name: '20' }
+      ],
+      majors: {},
+      form: {
+        sno: '',
+        name: '',
+        password: '',
+        nick: '',
+        sex: -1,
+        birthday: '',
+        photoAddress: null,
+        grade: '',
+        college: null,
+        major: '',
+        phone: '',
+        email: '',
+        createdBy: null,
+        createTime: ''
+      },
+      rules: {
+        sno: [
+          { required: true, message: '请填写学号信息', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度为11位', trigger: 'blur' }
+        ],
+        name: [
+          { required: true, message: '请填写姓名', trigger: 'blur' },
+          { min: 2, max: 4, message: '长度为2-4位', trigger: 'blur' }
+        ],
+        nick: [
+          { required: true, message: '请填写昵称', trigger: 'blur' },
+          { min: 1, max: 8, message: '长度为1-8位', trigger: 'blur' }
+        ],
+        sex: [
+          { required: true, message: '请选择性别', trigger: 'change' }
+        ],
+        birthday: [
+          { required: true, message: '请选择出生年月日', trigger: 'change' }
+        ],
+        grade: [
+          { required: true, message: '请选择年级', trigger: 'change' }
+        ],
+        college: [
+          { required: true, message: '请选择院系', trigger: 'change' }
+        ],
+        major: [
+          { required: true, message: '请选择专业', trigger: 'change' }
+        ],
+        phone: [
+          { required: true, trigger: 'blur', validator: validPhone }
+        ],
+        email: [
+          { required: true, message: '请输入您的邮箱', trigger: 'blur' }
+          // { required: true, trigger: 'blur', validator: validEmail }
+        ],
+        password: [
+          { required: true, message: '请填写密码', trigger: 'blur' },
+          { min: 6, max: 6, message: '长度为6位', trigger: 'blur' }
+        ]
+      },
+      // 图片上传
+      imageUrl: '',
+      // 图片上传，回显
+      formData: new FormData(),
+      imgs: {},
+      imgLen: 0,
+      // 图片base64编码
+      baseData: '',
+      baseResultUrl: '',
+      colleges: null
     }
   },
   methods: {
@@ -503,7 +664,13 @@ export default {
       }
     },
     handleEditUser (index, row) {
-
+      if (this.uid === null) {
+        this.uid = row.id
+        this.loadMy()
+      } else {
+        this.uid = null
+      }
+      // location.reload()
     },
     handleDeleteUser (index, row) {
       var id = row.id
@@ -591,8 +758,185 @@ export default {
     },
     exit () {
       localStorage.setItem('manage', null)
-      this.$router.go(-1)
-      this.$router.push('/main')
+      // this.$router.go(-1)
+      this.$router.push('/home')
+      location.reload()
+    },
+
+    // 用户信息编辑
+    // 图片上传
+    addImg (event) {
+      var _this = this
+      var reader = new FileReader()
+      const inputDOM = this.$refs.inputer
+      // 通过DOM取文件数据
+      this.fil = inputDOM.files
+      // 使用该对象读取file文件
+      reader.readAsDataURL(this.fil[0])
+
+      // 校验图片
+      const oldLen = this.imgLen
+      const len = this.fil.length + oldLen
+      if (len > 1) {
+        alert('最多可上传1张')
+        return false
+      }
+      for (let i = 0; i < this.fil.length; i++) {
+        const size = Math.floor(this.fil[i].size / 1024)
+        if (size > 5 * 1024 * 1024) {
+          alert('请选择5M以内的图片！')
+          return false
+        }
+        this.imgLen++
+        this.$set(this.imgs, this.fil[i].name + '?' + new Date().getTime() + i, this.fil[i])
+      }
+      // 读取文件成功后执行的方法函数
+      reader.onload = function (e) {
+        _this.baseData = e.target.result
+        console.log(_this.baseData)
+        axios.post('users/img', {
+          baseData: _this.baseData
+        }, {
+          'Content-Type': 'application/json', charset: 'UTF-8'
+        }).then(resp => {
+          if (resp.data.code === 200) {
+            _this.baseResultUrl = resp.data.data
+          }
+        })
+      }
+    },
+    getObjectURL (file) {
+      var url = null
+      if (window.createObjectURL !== undefined) { // basic
+        url = window.createObjectURL(file)
+      } else if (window.URL !== undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file)
+      } else if (window.webkitURL !== undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file)
+      }
+      // url是http动态地址
+      return url
+    },
+    delImg (key) {
+      this.$delete(this.imgs, key)
+      this.imgLen--
+    },
+    submit () {
+      for (const key in this.imgs) {
+        const name = key.split('?')[0]
+        this.formData.append('multipartFiles', this.imgs[key], name)
+      }
+      this.$http.post('/opinion/feedback', this.formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }).then(res => {
+        this.alertShow = true
+      })
+    },
+    // 图片上传
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    submitForm (formName) {
+      this.form.photoAddress = this.baseResultUrl
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.form)
+          axios({
+            method: 'post',
+            url: 'users',
+            data: this.form
+          }).then(resp => {
+            if (resp.data.code === 200) {
+              this.handleEditUser(null, null)
+            } else {
+              // location.reload()
+              alert('该用户已注册！')
+            }
+          }).catch(error => error)
+        } else {
+          return false
+        }
+      })
+    },
+    // 头像上传
+    handleAvatarSuccess (res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+      console.log('handleAvatarSuccess--this.imageUrl:' + this.imageUrl)
+    },
+    beforeAvatarUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    loadColleges () {
+      axios({
+        method: 'get',
+        url: 'college/datas',
+        data: this.form
+      }).then(resp => {
+        if (resp.data.code === 200) {
+          this.colleges = resp.data.data
+          console.log('------------------')
+          console.log(this.colleges)
+        }
+      }).catch(error => error)
+    },
+    loadMajor () {
+      console.log('loadmajor: ')
+      console.log(this.form.college)
+      if (this.form.college !== null) {
+        var _this = this
+        axios({
+          method: 'get',
+          url: 'major/datas/%7Bcid%7D',
+          params: {
+            cid: this.form.college
+          }
+        }).then(resp => {
+          if (resp.data.code === 200) {
+            _this.majors = resp.data.data
+            console.log('_this.majors:' + _this.majors)
+            console.log(_this.majors)
+          }
+        }).catch(error => error)
+      } else {
+        alert('您还没有选择院系呢！')
+      }
+    },
+    // 加载当前需要编辑的用户数据
+    loadMy () {
+      var _this = this
+      axios({
+        method: 'post',
+        url: 'users/datas/%7Buid%7D',
+        params: {
+          uid: this.uid
+        }
+      }).then(resp => {
+        if (resp.data.code === 200) {
+          _this.form = resp.data.data
+          _this.major = _this.form.major.major
+          _this.form.major = _this.form.major.id
+          console.log(_this.form)
+          this.loadCollegeName()
+        }
+      }).catch(error => error)
+    }
+  },
+  mounted: function () {
+    this.loadColleges()
+  },
+  watch: {
+    'form.college' () {
+      this.form.major = ''
+      this.loadMajor()
     }
   },
   created () {
@@ -664,18 +1008,27 @@ export default {
 }
 .dataSearch li{
   width: 300px;
-  height: 100px;
+  height: 90px;
   float: left;
   margin: 0 5%;
 }
 .dataSearch svg{
-  line-height: 100px;
-  font-size: 80px;
+  line-height: 80px;
+  font-size: 64px;
 }
 .dataSearch p{
   padding-left: 20px;
   display: inline-block;
   vertical-align: top;
-  line-height: 84px;
+  line-height: 68px;
+}
+.fanhui{
+  margin-left: 12px;
+  margin-bottom: 20px;
+  padding: 3px 10px;
+  width: 33px;
+  height: 22px;
+  border: 2px solid #b3d4fc;
+  border-radius: 10px;
 }
 </style>
